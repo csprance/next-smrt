45eab1bcd5e240ca5175ec40bc95c8539a6ac1bb
Chris Sprance <chrissprance@gmail.com>
2019-01-07 00:36:20 -0500

    Changed site image

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,5 +3,5 @@ export const DEV = process.env.NODE_ENV !== 'production';
 export const SITE_NAME = 'Next-SMRT';
 export const SITE_TITLE = 'Next-SMRT boilerplate';
 export const SITE_DESCRIPTION = 'Next - Styled components/Material-ui/Redux/Typescript';
-export const SITE_IMAGE = '/static/site-image.png';
-export const SITE_AUTHOR = 'Chris Sprance';
+export const SITE_IMAGE = '/static/site-image.jpg';
+export const SITE_AUTHOR = 'cSprance';
