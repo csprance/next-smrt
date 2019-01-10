a36550e634ac3f178af271ea7774b0b40d32dcaa
Chris Sprance <chrissprance@gmail.com>
2019-01-09 20:46:45 -0500

    Added in workaround for withCSS and nextjs Link not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import JssProvider from 'react-jss/lib/JssProvider';
 import { Provider } from 'react-redux';
 import getPageContext from '../src/lib/getPageContext';
 import withReduxStore from '../src/lib/with-redux-store';
+import '../styles/fix-next-link.css'; // FIXME: https://github.com/zeit/next-plugins/issues/282
 import { SweetAlertSyle } from '../styles/GlobalStyles';
 
 class MyApp extends App {
