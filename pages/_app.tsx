ec15c1dfaddb4357aa385b2aef977f58d748dd05
Chris Sprance <chrissprance@gmail.com>
2019-06-26 23:20:57 -0400

    Updated Deps made changes for latest version of material-ui and styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,15 +12,21 @@ import { theme } from '../styles/styles';
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }: any) {
     let pageProps = {};
-
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
-
     return { pageProps };
   }
 
+  state = {
+    ready: false
+  };
+
   componentDidMount() {
+    // FIXME: https://github.com/styled-components/styled-components/issues/1860
+    this.setState({
+      ready: true
+    });
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
@@ -33,6 +39,7 @@ class MyApp extends App {
     const { Component, pageProps, reduxStore } = props;
     return (
       <Container>
+        {!this.state.ready && <div className="fouc" />}
         <Provider store={reduxStore}>
           {/* Material-UI Theme Provider */}
           <ThemeProvider theme={theme}>
@@ -43,6 +50,11 @@ class MyApp extends App {
             <Component {...pageProps} />
           </ThemeProvider>
         </Provider>
+        <style jsx global>{`
+          .fouc + * {
+            visibility: hidden !important;
+          }
+        `}</style>
       </Container>
     );
   }
