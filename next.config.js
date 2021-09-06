13410596e9eac759c35c3a62dfd217789829b199
csprance <chrissprance@gmail.com>
2021-09-05 20:40:15 -0400

    Updated to next 11 and some other deps

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ module.exports = {
 		API_URL: process.env.API_URL || "http://next-smrt.csprance.localhost/api",
 		API_TOKEN: process.env.API_TOKEN || "API_TOKEN"
 	},
-	future: {
-		webpack5: true,
-	},
+	webpack5: true,
 };
